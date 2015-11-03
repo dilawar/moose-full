56ab5541e16702ca5c618138d42cd890f648ed20
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-03 20:22:13 +0530

    This is tagged for both brew and macport:

--- macosx/brew_moose.sh
+++ macosx/brew_moose.sh
@@ -3,6 +3,7 @@ set -x
 
 rm -f /Library/Caches/Homebrew/moose*.tar.gz
 rm -f /Library/Caches/Homebrew/moose*.zip
+cp ./moose.rb /usr/local/Library/Formula/moose.rb
 
 brew uninstall moose
 brew -v install moose --debug | tee _build.log 
