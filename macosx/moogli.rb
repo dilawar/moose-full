8193b48e819134a74587c87d9764d4adc637cc4a
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-06 00:22:07 +0530

    Added two scripts.

---
+++ macosx/moogli.rb
@@ -0,0 +1,18 @@
+class Moogli < Formula
+  desc "3d visualizer of neuronal simulation"
+  homepage "http://moose.ncbs.res.in/moogli"
+  url "https://github.com/BhallaLab/moogli/archive/master.tar.gz"
+  version "0.5.0"
+
+  depends_on "open-scene-graph"
+  depends_on "python" if MacOS.version <= :snow_leopard
+  depends_on "pyqt"
+
+  def install
+    system "python", *Language::Python.setup_install_args(prefix)
+  end
+
+  test do
+    system "#{HOMEBREW_PREFIX}/bin/python", "-c", "import moogli"
+  end
+end
