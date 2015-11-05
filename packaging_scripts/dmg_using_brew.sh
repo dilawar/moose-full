55c93ece1af50a343a62a49cf50596dd9b88feab
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-06 01:51:02 +0530

    Once brew has installed, use this to create dmg file.

--- packaging_scripts/dmg_using_brew.sh
+++ packaging_scripts/dmg_using_brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 set -x
-sudo brew install moose
-sudo brew install moogli
+brew install moose
+brew install moogli
 sudo hdiutil create ~/Desktop/moose-3.0.2.dmg -srcdevice /usr/local -volname MOOSE
