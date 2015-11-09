dbbcfe2a76aa05d3c6532c2a5e28a7a4f1b642db
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-09 19:41:38 +0530

    Added PyQt4 dependency.

--- macosx/moogli.rb
+++ macosx/moogli.rb
@@ -4,12 +4,21 @@ class Moogli < Formula
   url "https://github.com/BhallaLab/moogli/archive/master.tar.gz"
   version "0.5.0"
 
+  resource "PyQt4" do
+    url "http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt-mac-gpl-4.11.4.tar.gz"
+    sha256 "f178ba12a814191df8e9f87fb95c11084a0addc827604f1a18a82944225ed918"
+  end
+
   depends_on "open-scene-graph"
   depends_on "python" if MacOS.version <= :snow_leopard
-  depends_on "pyqt"
   depends_on "gcc"
 
   def install
+    resource("PyQt4").stage do
+      system "python", "configure-ng.py", "--confirm-license"
+      system "make"
+    end
+
     ENV['CC'] = "#{HOMEBREW_PREFIX}/bin/gcc-5"
     ENV['CXX'] = "#{HOMEBREW_PREFIX}/bin/g++-5"
     system "python", "setup.py", "build"
