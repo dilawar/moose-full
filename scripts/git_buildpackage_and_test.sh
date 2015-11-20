a538a7d3d274ae5e2a5062a4f50761793b551936
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-20 19:04:07 +0530

    Let's test this one
    Using tags.

--- scripts/git_buildpackage_and_test.sh
+++ scripts/git_buildpackage_and_test.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 set +e
 set -x
+function build_deb
+{
+    echo "Building for $2 with arch $1"
+    git buildpackage --git-ignore-new --git-no-sign-tags --git-dist=$1 --git-arch=$2
+    #git buildpackage --git-ignore-new --git-no-sign-tags --git-pbuilder --git-dist=$1 --git-arch=$2
+}
+
 export PYTHONPATH=""
-ARCH=amd64
-DIST=wheezy
-git buildpackage --git-ignore-new --git-no-sign-tags --git-dist=$DIST \
-    --git-arch=$ARCH
+build_deb wheezy amd64
+build_deb wheezy i386
+
 echo "installing"
-sudo dpkg -i ../*.deb
+sudo dpkg -i ../*amd64*.deb
 python -c 'import moose'
 python -c 'import moogli'
 echo "now uninstalling"
-sudo apt-get remove moose-core moose-doc moose-gui moose-moogli moose-moogli
+sudo apt-get remove moose 
 echo "Checking if moose-python is still there"
 python -c 'import moose' || echo "Successfully uninstalled"
 python -c 'import moogli' || echo "Successfully uninstalled"
