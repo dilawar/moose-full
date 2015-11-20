8eaa19398e35995f74284ac70243842281ecf908
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-20 17:23:07 +0530

    Script to create debian package locally.

---
+++ packaging_scripts/packages_local_using_cmake.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+PKG_PATH=`pwd`/moose-3.0.2-Linux-amd64.deb
+
+if [ ! -f $PKG_PATH ]; then
+    echo "Building package"
+    (
+        cd ..
+        mkdir -p _build
+        cd _build
+        cmake -DCMAKE_INSTALL_PREFIX=_install ..
+        make -j`nproc` | tee $0.log
+        cpack -GDEB -DCPACK_SET_DESTDIR=ON 
+    )
+    cp ../_build/*.deb .
+else
+    echo "$PKG_PATH exists"
+fi
+
+echo " Extracting "
+
+TEMPDIR=/tmp/moose_debian
+if [ ! -d "$TEMPDIR/usr" ]; then
+    dpkg-deb -x $PKG_PATH $TEMPDIR
+fi
+
+(
+    cd $TEMPDIR
+    find . -type f
+    find . -type f | grep "_build\/_install"
+)
+#rm -rf $TEMPDIR
