3d93826b13b0342c044b8b17dc6638f69213661c
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-22 01:10:39 +0530

    If static gsl or static sbml are to be used, pass GSL_STATIC_HOME or SBML_STATIC_HOME.

--- build_static_gsl.cmake
+++ build_static_gsl.cmake
@@ -5,16 +5,14 @@ SET(GSL_INSTALL_DIR ${CMAKE_BINARY_DIR}/__gsl_install)
 FILE(MAKE_DIRECTORY ${GSL_INSTALL_DIR})
 MESSAGE("++ GSL_SRC_DIR := ${GSL_SRC_DIR}")
 
-set(STATIC_GSL_LIBRARY ${GSL_INSTALL_DIR}/lib/libgsl.a)
-set(STATIC_GSLBLAS_LIBRARY ${GSL_INSTALL_DIR}/lib/libgslcblas.a)
-
 SET(GSL_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/__gsl_build__)
 FILE(MAKE_DIRECTORY ${GSL_BUILD_DIR})
 
 # NOTE: Set WORKING_DIRECTORY to GSL_SRC_DIRECTORY else the build might fail
 # because of missing texinfo and texi2html package.
+SET(GSL_OUTPUT_BIN ${GSL_INSTALL_DIR}/bin/gsl-config)
 ADD_CUSTOM_COMMAND(
-    OUTPUT ${STATIC_GSLBLAS_LIBRARY} ${STATIC_GSL_LIBRARY}
+    OUTPUT ${GSL_OUTPUT_BIN}
     COMMAND ${GSL_SRC_DIR}/configure --prefix=${GSL_INSTALL_DIR} --with-pic --enable-static --disable-shared
     COMMAND $(MAKE) 
     COMMAND $(MAKE) install
@@ -23,14 +21,13 @@ ADD_CUSTOM_COMMAND(
     )
 
 ADD_CUSTOM_TARGET(_libgsl ALL 
-    DEPENDS ${STATIC_GSL_LIBRARY} ${STATIC_GSLBLAS_LIBRARY}
+    DEPENDS ${GSL_OUTPUT_BIN}
     )
 
 set(GSL_INCLUDE_DIR ${GSL_INSTALL_DIR}/include)
-message("+++ GSL static libs ${STATIC_GSL_LIBRARY} ${STATIC_GSLBLAS_LIBRARY}")
-# Next time moose module must be able to find it.
+
+# Next time moose module must be able to find static libraries.
 set(ENV{GSL_HOME} ${GSL_INSTALL_DIR})
-LIST(APPEND ALL_STATIC_LIBS ${STATIC_GSLBLAS_LIBRARY} ${STATIC_GSL_LIBRARY})
 
 ## Ofcourse, moose-full depends on it.
 add_dependencies(_moose_static_dependencies _libgsl)
