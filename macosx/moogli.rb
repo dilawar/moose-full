033ed0cd46fda3ed1bb5c951772c741e4367efd7
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-10 13:38:26 +0530

    Script to build macosx package locally.

--- macosx/moogli.rb
+++ macosx/moogli.rb
@@ -1,25 +1,24 @@
 class Moogli < Formula
   desc "3d visualizer of neuronal simulation"
   homepage "http://moose.ncbs.res.in/moogli"
-  url "https://github.com/BhallaLab/moogli/archive/macosx.tar.gz"
+  url "https://github.com/BhallaLab/moogli/archive/master.tar.gz"
   version "0.5.0"
 
-  depends_on "open-scene-graph"
+  depends_on "cmake" => :build
   depends_on "python" if MacOS.version <= :snow_leopard
+  depends_on "open-scene-graph"
   depends_on "sip"
   depends_on "pyqt"
+  depends_on "moose"
 
   def install
-    ENV['CFLAGS'] = ""
-    ENV['CXXFLAGS'] = ""
-    # Copy QtCore.so etc to lib of our app.
-    system "./build_on_macosx.sh"
-    system "python", "setup.py", "install", "--prefix", prefix
-    (prefix/"lib/python2.7/site-packages/moogli/core").install "moogli/core/_moogli.so"
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "python", "moogli/cmake_modules/setup.py", "install", \
+        "--prefix=#{prefix}", "--record=installed.txt"
   end
 
   test do
-    system "#{HOMEBREW_PREFIX}/bin/python", "-c", "import moogli"
     system "python", "-c", "import moogli"
   end
 end
